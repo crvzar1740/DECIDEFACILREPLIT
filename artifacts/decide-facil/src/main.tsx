f27385c9f4bb6ecd406a5439fa24357475fca99f
crvzar1740 <claudiovisentin@gmail.com>
2026-06-02 19:42:28 -0300

    Add URL redirection handling in main.tsx

--- artifacts/decide-facil/src/main.tsx
+++ artifacts/decide-facil/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(
+    null,
+    "",
+    import.meta.env.BASE_URL.replace(/\/$/, "") + redirect
+  );
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
